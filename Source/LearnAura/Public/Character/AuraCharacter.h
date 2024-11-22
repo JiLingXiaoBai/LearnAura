259e0e43ddd2e5c8e8f6a5015772e2c9ad582add
JiLingXiaoBai <uiojkl3588@qq.com>
2024-11-22 22:05:37 +0800

    Player and Enemy Characters

---
+++ Source/LearnAura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraCharacterBase.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEARNAURA_API AAuraCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/LearnAura/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,19 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS(Abstract)
+class LEARNAURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+};
